33da47162e72376786402f7235eda9d0917f6ede
pkdhirasaria <pkdhirasaria@gmail.com>
2016-10-23 16:01:35 +0530

    Update GCD.java

--- GCD.java
+++ GCD.java
@@ -1,4 +1,35 @@
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+/*import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.JavascriptExecutor;*/
+public class gcd  {
+    public static void main(String[] args) {
+        
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "file:///home/ise/gcd.html";
+        
+        driver.get(baseUrl);
+       	WebElement email1 = driver.findElement(By.id("num1"));
+       	email1.sendKeys("785");
+       	WebElement email2 = driver.findElement(By.id("num2"));
+       	email2.sendKeys("632");
+       	WebElement element = driver.findElement(By.id("calc"));
+       	element.click();
+	
+        System.exit(0);
+    }
+}
+
+
+
+
+
+/*import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.By;
@@ -41,4 +72,4 @@ public class myclass {
   //      System.exit(0);
     }
 
-}
+}*/
